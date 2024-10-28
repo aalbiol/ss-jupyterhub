03ebb68e6c7dcfd8de2413427b17dc528433cd08
Antonio Albiol <aalbiol@upv.es>
2024-10-28 11:12:59 +0100

    Update

--- source/LabSS-2023/analisis_logs/logjupyteranalysis.py
+++ source/LabSS-2023/analisis_logs/logjupyteranalysis.py
@@ -35,8 +35,7 @@ def analisis_linea(linea):
     #time.sleep(.1)
     return campo_fecha,campo_hora,usuario,dir_ip
 
-
-def detecta_usuarios_dobles(ficherologs):
+def carga_tabla(ficherologs):
     with open(ficherologs, 'r') as file1:
         lineas = file1.readlines()
     fechas=[]
@@ -50,13 +49,22 @@ def detecta_usuarios_dobles(ficherologs):
         if res is None:
             continue
         fecha,hora,usuario,dir_ip=res
+        hora=hora[:5]
+        #print(fecha,hora)
         fechas.append(fecha)
-        horas.append(horas)
+        horas.append(hora)
         usuarios.append(usuario)
         ips.append(dir_ip)
         data.append({"fecha":fecha,"hora":hora,"usuario":usuario, "ip":dir_ip})
 
     df=pd.DataFrame(data)
+
+    df['hora_dt']=pd.to_datetime(df['hora'],format="%H:%M")
+    return df
+
+def detecta_usuarios_dobles(ficherologs):
+
+    df=carga_tabla(ficherologs)
 ############ Solucion con pandas
     #df1=df.groupby(["fecha","ip"]).usuario.unique().tolist()
     #casos_pandas=[c for c in df1 if len(c)>1]
@@ -69,12 +77,33 @@ def detecta_usuarios_dobles(ficherologs):
         usuarios_por_ip[(evento.fecha,evento.ip)]=set()
     for k in range(len(df)):
         evento=df.iloc[k]
-        usuarios_por_ip[(evento['fecha'],evento.ip)].add(evento.usuario)   
+        usuarios_por_ip[(evento['fecha'],evento.ip)].add((evento.usuario))   
 
     casos_interesantes={}
     for k,v in usuarios_por_ip.items():
         if len(v)>1:
             casos_interesantes[k]=v
+
+    for k,v in casos_interesantes.items():
+        fecha=k[0]
+        ip=k[1]
+        usuarios=v
+        horas_usuario={}
+        for u in usuarios:
+            horas_usuario[u]=[]
+        for k in range(len(df)):
+            evento=df.iloc[k]
+            if evento.fecha != fecha :
+                continue
+            if evento.ip != ip:
+                continue
+            evento.usuario
+            if evento.usuario in usuarios:
+                horas_usuario[evento.usuario].append(evento.hora_dt)
+        print("=========================")
+        for k,v in horas_usuario.items():
+            print(k,min(v),max(v))
+        
     return casos_interesantes
 
 
@@ -92,9 +121,11 @@ def detecta_usuarios_dobles_hora(ficherologs):
         res=analisis_linea(l)
         if res is None:
             continue
-        fecha,hora,usuario,dir_ip=res
+        fecha,tiempo,usuario,dir_ip=res
+        hora=int(tiempo.split(':')[0])
+
         fechas.append(fecha)
-        horas.append(horas)
+        horas.append(hora)
         usuarios.append(usuario)
         ips.append(dir_ip)
         data.append({"fecha":fecha,"hora":hora,"usuario":usuario, "ip":dir_ip})
